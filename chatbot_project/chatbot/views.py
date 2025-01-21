2ad969a2d386921e6997993c417816db03b9919f
Danish <mahajandanish0508@gmail.com>
2025-01-21 10:44:54 +0000

    make backend and frontend

--- chatbot_project/chatbot/views.py
+++ chatbot_project/chatbot/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+# from Pipeline.pipe import Pipeline
 
-# Create your views here.
+def query_view(request):
+    if request.method == "POST":
+        user_query = request.POST.get('query')
+        response = "working fine"  # for testing
+        # response = Pipeline().process_query(user_query)
+        return JsonResponse({'response': response})
+    return render(request, 'chatbot/query_form.html')
